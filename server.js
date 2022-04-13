f5cf550b9947dc3e13796023de28fefea4d8f287
nng32 <nng32@my.bcit.ca>
2022-04-13 11:09:40 -0700

    Update URLs

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const unicornSchema = new mongoose.Schema({
 
 const unicornModel = mongoose.model("unicorns", unicornSchema);
 
-app.listen(5000, err => {
+app.listen(process.env.PORT || 5000, err => {
     if (err) console.log(err);
 })
 
